4eace3dbd6b03e6f1691fdac0654d1f8351a03d2
yosuke026 <y3kh6s03@gmail.com>
2024-01-24 15:47:50 +0900

    header fix

--- src/header/header.php
+++ src/header/header.php
@@ -4,6 +4,7 @@ namespace header;
 
 function header()
 {
+  $date = $_POST["datetime-local"] ?? date("Y-m-d");
 ?>
   <!DOCTYPE html>
   <html lang="ja">
@@ -11,6 +12,7 @@ function header()
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" href="./header/css/header.css">
     <link rel="stylesheet" type="text/css" href="graph/css/style.css">
@@ -18,9 +20,8 @@ function header()
     <link rel="stylesheet" type="text/css" href="task/css/style.css">
     <title>ちょうどいいタスク管理アプリ</title>
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
-    <!-- <script src="https://cdn.jsdelivr.net/npm/flatpickr/dist/l10n/en.js"></script> -->
+    <script src="https://npmcdn.com/flatpickr/dist/flatpickr.min.js" defer></script>
+    <script src="https://npmcdn.com/flatpickr/dist/l10n/en.js" defer></script>
     <script src="./header/js/header.js" type="module" defer></script>
   </head>
 
@@ -30,12 +31,12 @@ function header()
       <div class="header-logo">
         <img src="./header/images/dev2logo.svg" alt="header-image">
       </div>
-      <div class="cal-container">
-        <input class="cal" id="myCal" type="form-control" name="datetime-local">
-        <span class="cal-date"></span>
-      </div>
+      <form class="cal-container" action="index.php" method="POST">
+          <input class="cal" id="myCal" type="text" name="datetime-local">
+          <span class="cal-date"><?php echo $date?></span>
+      </form>
       <div class="header-username">
-        <span class="header-username-span">達人くん</span>
+        <span class="header-username-span"></span>
       </div>
       <div class="header-humburger">
         <span class="header-humburger-top"></span>
