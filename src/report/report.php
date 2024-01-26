608a37f2a8469bab3233a4ea31292e9adc69c2eb
yosuke026 <y3kh6s03@gmail.com>
2024-01-26 11:19:47 +0900

    実績のHTMLとCSSだけ作成

--- src/report/report.php
+++ src/report/report.php
@@ -1,15 +1,107 @@
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-  <meta charset="UTF-8">
-  <link rel="stylesheet" type="text/css" href="css/style.css">
-</head>
-
-<body>
-  <div id="wrapper">
-    <div>report</div>
+<?php  ?>
+<div class="report">
+  <div class="report-header">
+    <span class="report-header-title">
+      Task List
+    </span>
+    <span class="report-header-totaltime">
+      総学習時間: 00:00:00
+    </span>
   </div>
-</body>
-
-</html>
+  <form class="report-inner" method="form">
+    <ul class="report-ul">
+      <li class="report-li">
+        <div class="report-li-top">
+          <span class="rank">
+            1
+          </span>
+          <span class="task-name">
+            提出Quest DB/SQL
+          </span>
+        </div>
+        <div class="report-li-bottom">
+          <div class="report-li-bottom-inner">
+            <label for="daily-time">時間</label>
+            <input class="daily-time" name="daily-time" id="daily-time" value="3:00">
+            </input>
+          </div>
+          <span class="totaltime">
+            合計時間: 14:00:00
+          </span>
+        </div>
+      </li>
+      <li class="report-li">
+        <div class="report-li-top">
+          <span class="rank">
+            1
+          </span>
+          <span class="task-name">
+            提出Quest DB/SQL
+          </span>
+        </div>
+        <div class="report-li-bottom">
+          <div class="report-li-bottom-inner">
+            <label for="daily-time">時間</label>
+            <input class="daily-time" name="daily-time" id="daily-time" value="3:00">
+            </input>
+          </div>
+          <span class="totaltime">
+            合計時間: 14:00:00
+          </span>
+        </div>
+      </li>
+      <li class="report-li">
+        <div class="report-li-top">
+          <span class="rank">
+            1
+          </span>
+          <span class="task-name">
+            提出Quest DB/SQL
+          </span>
+        </div>
+        <div class="report-li-bottom">
+          <div class="report-li-bottom-inner">
+            <label for="daily-time">時間</label>
+            <input class="daily-time" name="daily-time" id="daily-time" value="3:00">
+            </input>
+          </div>
+          <span class="totaltime">
+            合計時間: 14:00:00
+          </span>
+        </div>
+      </li>
+      <li class="report-li">
+        <div class="report-li-top">
+          <span class="rank">
+            1
+          </span>
+          <span class="task-name">
+            提出Quest DB/SQL
+          </span>
+        </div>
+        <div class="report-li-bottom">
+          <div class="report-li-bottom-inner">
+            <label for="daily-time">時間</label>
+            <input class="daily-time" name="daily-time" id="daily-time" value="3:00">
+            </input>
+          </div>
+          <span class="totaltime">
+            合計時間: 14:00:00
+          </span>
+        </div>
+      </li>
+    </ul>
+    <div class="report-description">
+      <label for="report-textarea">
+        今日の振り返り
+      </label>
+      <div class="report-description-inner">
+        <textarea name="text" id="report-textarea" cols="30" rows="10"></textarea>
+        <div class="button-area">
+          <button>保存</button>
+          <button>投稿</button>
+        </div>
+      </div>
+    </div>
+  </form>
+</div>
